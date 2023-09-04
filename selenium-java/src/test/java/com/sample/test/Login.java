bc58d331a0d28d3365c57780d8200d6de369bd7a
shivakumarpathipaka <shivakumarpathipaka@shivas-mbp.lan>
2023-09-03 21:20:53 -0400

    using testng.xml

--- selenium-java/src/test/java/com/sample/test/Login.java
+++ selenium-java/src/test/java/com/sample/test/Login.java
@@ -1,14 +1,15 @@
 package com.sample.test;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Login {
@@ -31,12 +32,12 @@ public class Login {
 	@Test
 	public void launch() {
 		driver = new ChromeDriver();
-		System.out.println(prop.getProperty("URL"));
 		driver.get(prop.getProperty("URL"));
 
 	}
 
 	@AfterMethod
+	@AfterTest
 	public void tearDown() {
 		if (driver != null) {
 			driver.quit();
